d95ba02ff6c3268c2fbbc4ad358752e07551f237
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2014-05-21 08:52:31 -0700

    move CMF to imf module

--- cmf.py
+++
@@ -1,7 +0,0 @@
-#simple exploration of the effects of the cmf on
-#inferred SFHs
-
-import numpy as np
-from imf import IMF
-#threshold = #number of clusters per unit mass considered to be 'dangerous'
-
