83c3e402a0c0e89b38550d9624d49119ea2ec1d8
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2015-03-17 21:55:55 -0400

    add suport for parsec isochrones

--- isochrone.py
+++ isochrone.py
@@ -80,7 +80,7 @@ class Padova2007(Isochrone):
                 self.pars = np.hstack([self.pars, pars])
             self.max_mass.append(pars['MASSIN'].max())
             
-        self.logage_list = np.sort(np.unique(pars['LOGAGE']))
+        self.logage_list = np.sort(np.unique(self.pars['LOGAGE']))
                 
     def load_one_isoc(self, Z): 
         """Given a metallicity (absolute, i.e. Z_sun = 0.019) read the
@@ -114,6 +114,7 @@ class Geneva2013(Isochrone):
             Initial rotation speed, in fraction of V_breakup.  Valid
             values are 0.00 and 0.400
         """
+        self.pars = None
         self.Vini = Vini
         flist = glob.glob(self.isocdir+'Isoc*Vini{0:3.2f}*.dat'.format(Vini))
         if len(flist) is 0 :
@@ -138,7 +139,7 @@ class Geneva2013(Isochrone):
                     self.pars = np.hstack([self.pars, pars])
                 self.max_mass.append(pars['MASSIN'].max())
 
-    def load_one_isoc(self, Z, logt):
+    def load_one_isoc(self, Z, logage):
         """M_ini Z_ini OmOc_ini M logL logTe_c logTe_nc MBol MV U-B
         B-V B2-V1 r_pol oblat g_pol Omega_S v_eq v_crit1 v_crit2
         Om/Om_cr lg(Md) lg(Md_M) Ga_Ed H1 He4 C12 C13 N14 O16 O17 O18
@@ -146,12 +147,12 @@ class Geneva2013(Isochrone):
         """
         Z = float(Z)
 
-        fn = 'Isochr_Z{0:4.3f}_Vini{1:3.2f}_t{2:06.3f}.dat'.format(Z, self.Vini, logt)
+        fn = 'Isochr_Z{0:4.3f}_Vini{1:3.2f}_t{2:06.3f}.dat'.format(Z, self.Vini, logage)
         filename = glob.glob(self.isocdir + fn)
         mini, zini, mact, logl, logt, logt_nc, mbol, rpol, logg = np.loadtxt(filename[0], skiprows=1,
                                                                     usecols=(0,1,3,4,5,6,7,12,14),
                                                                     unpack=True)
-        tt = np.zeros_like(mini) + logt
+        tt = np.zeros_like(mini) + logage
         pars = np.vstack([tt, mini, logl, logt, logg, zini, mact])
         pars =  self.structure_array(pars.T, ['LOGAGE','MASSIN','LOGL',
                                               'LOGT','LOGG','Z','MASSACT'])
@@ -167,12 +168,43 @@ class MIST(Isochrone):
         self.Zlegend = None
         self.Vini = Vini
 
-class Parsec(Isochrone):
-
+class Parsec(Padova2007):
+    """Parsec isochrones.
+    """
+    hrdspydir, f = os.path.split(__file__)
+    isocdir = hrdspydir + '/data/isochrones/parsec/'
     
-    def __init__(self):
-        """Parsec isochrones.
+    def __init__(self, OV=0.5):
         """
-        self.Zlist = None
-        self.Zlegend = None
-        self.Vini = Vini
+        :param OV:
+            Convective core overshooting.  Valid values are 0.3, 0.5, 0.7.
+        """
+        self.pars = None
+        self.OV = OV
+        flist = glob.glob(self.isocdir+'isoc*OV{0:2.1f}*.dat'.format(OV))
+        if len(flist) is 0 :
+            raise NameError('nothing returned for ls '
+                            '{0}isoc*OV{1:2.1f}*.dat'.format(self.isocdir, Vini))
+        Z_legend = [f[f.rfind('z0')+1:f.rfind('z0')+6] for f in flist]
+        self.Z_list = np.unique(np.array([ float(z) for z in Z_legend]))
+        self.Z_legend = ['{0:4.3f}'.format(z) for z in self.Z_list]
+
+    def load_one_isoc(self, Z): 
+        """Given a metallicity (absolute, i.e. Z_sun = 0.019) read the
+        corresponding isochrone data, and use it to produce a
+        structured array of stellar parameters.
+        """
+
+        #Z	log(age/yr)	M_ini   	M_act	logL/Lo	logTe	logG	mbol    F200LP1 F218W1  F225W1  F275W1  F336W1  F350LP1 F390W1  F438W1  F475W1  F555W1  F600LP1 F606W1  F625W1  F775W1  F814W1  F850LP1	C/O	M_hec	period	pmode	logMdot	slope	int_IMF	stage
+        Z = float(Z)
+        fn = '{0}isoc_z{1:4.3f}_OV{2:2.1f}.dat'.format(self.isocdir, Z, self.OV)
+        filename = glob.glob(fn)
+        zz, age, mini, mact, logl, logt, logg, mbol, composition, phase = np.loadtxt(filename[0],
+                                                                           usecols=(0,1,2,3,4,5,6,7, 24, 31),
+                                                                           unpack=True)
+        pars = np.vstack([age, mini, logl, logt, logg, zz, phase, mact, composition])
+        pars =  self.structure_array(pars.T, ['LOGAGE','MASSIN','LOGL',
+                                              'LOGT','LOGG','Z','PHASE','MASSACT','COMP'])
+
+        return pars
+
